92a9301817c38b1fd8cc9f297312d970fb5fe5b9
aayalac <aayalac@misena.edu.co>
2022-09-13 17:45:45 -0500

    Agregar archivos de proyecto.

---
+++ POO/Circulo.cs
@@ -0,0 +1,9 @@
+﻿class Circulo
+{
+    const double pi = 3.1416; // propiedad de la Clase Círculo. Campos de clase.
+
+    public double calculoArea(int radio) // método de clase. ¿que pueden hacer los objetos de tipo círculo?
+    {
+        return pi * radio * radio;
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo miCirculo; // Creacion de objeto de tipo Círculo. Variable / objeto de tipo círculo.
+
+            miCirculo = new Circulo(); // Iniciación de variable / objeto de tipo circulo. Instanciar una clase (instanciacion, ejemplarizacion, creacion de ejemplar de clase).
+
+            Console.WriteLine(miCirculo.calculoArea(5));
+
+            Circulo miCirculo2 = new Circulo();
+
+            Console.WriteLine(miCirculo2.calculoArea(9));
+        }
+    }
+}
