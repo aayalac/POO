55e348eb4e0dd242a3f43275e0e278be465897d0
aayalac <aayalac@misena.edu.co>
2022-09-15 05:55:30 -0500

    generar constructor a la clase Coche

---
+++ POO/Coche.cs
@@ -0,0 +1,38 @@
+﻿namespace POO
+{
+    internal class Coche
+    {
+        private int ruedas;
+
+        private double largo;
+
+        private double ancho;
+
+        private bool climatizador;
+
+        private string? tapiceria;
+
+        public Coche() // constructor (estado inicial)
+        {
+            ruedas = 4;
+
+            largo = 2300.5;
+
+            ancho = 0.800;
+        }
+
+        public Coche(double largoCoche, double anchoCoche) // sobrecarga
+        {
+            ruedas = 4;
+
+            largo = largoCoche;
+
+            ancho = anchoCoche;
+        }
+
+        public string getInfoCoche()
+        {
+            return "Informacion del coche:\n" + "Ruedas: " + ruedas + " " + "Largo: " + ruedas + " " + "Ancho: " + ancho;
+        }
+    }
+}
--- POO/Program.cs
+++ POO/Program.cs
@@ -4,12 +4,24 @@
     {
         static void Main(string[] args)
         {
-           ConversorEuroDolar obj = new ConversorEuroDolar();
+           /*ConversorEuroDolar obj = new ConversorEuroDolar(); // Instanciado.
 
             obj.CambiaValorEuro(1.5);
 
-           Console.WriteLine(obj.Convierte(50));
+           Console.WriteLine(obj.Convierte(50));*/
 
+        Coche coche1 = new Coche(); // Crear objeto / instancia de tipo coche.
+            // Dar un estado inicial a nuestro coche
+
+        Coche coche2 = new Coche();
+
+        Console.WriteLine(coche1.getInfoCoche());
+
+        Console.WriteLine(coche2.getInfoCoche());
+
+        // llamado al metodo
+        Coche coche3 = new Coche(4500.25, 1200.35);    
+        Console.WriteLine(coche3.getInfoCoche()); 
         }
     }
 }
