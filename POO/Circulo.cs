48b1d728435743e2af3c3278b8ea6afdcf3197a5
aayalac <aayalac@misena.edu.co>
2022-10-19 21:23:04 -0500

    cambios en la clase Circulo

--- POO/Circulo.cs
+++ POO/Circulo.cs
@@ -1,8 +1,8 @@
 ﻿class Circulo
 {
-    private const double pi = 3.1416; // propiedad de la Clase Círculo. Campos de clase.
+    private const double pi = 3.1416; // Propiedad de la Clase Círculo. Campos de clase.
 
-    public double CalculoArea(int radio) // método de clase. ¿que pueden hacer los objetos de tipo círculo?
+    public double CalculoArea(int radio) // Método de clase. ¿que pueden hacer los objetos de tipo círculo?
     {
         return pi * radio * radio;
     }
